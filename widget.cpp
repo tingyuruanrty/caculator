f8676144bd317bbb96495c54528a556405e5953b
Tingyu Ruan <tingyuruanrty@gmail.com>
2024-08-15 19:52:08 -0700

    the interface for caculator finished, algorithm for evaluate still haven't finished yet

---
+++ widget.cpp
@@ -0,0 +1,152 @@
+#include "widget.h"
+#include "./ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("caculator");
+    QFont f("SimSun",20);
+    ui->mainLineEdit->setFont(f);
+
+    QIcon a("C:\\Users\\tingy\\Desktop\\c++\\qtprojects\\caculator\\arrow.png");
+    ui->backSpaceButton->setIcon(a);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_oneButton_clicked()
+{
+    expression+="1";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_twoButton_clicked()
+{
+    expression+="2";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_threeButton_clicked()
+{
+    expression+="3";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_fourButton_clicked()
+{
+    expression+="4";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_fiveButton_clicked()
+{
+    expression+="5";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_sixButton_clicked()
+{
+    expression+="6";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_sevenButton_clicked()
+{
+    expression+="7";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_eightButton_clicked()
+{
+    expression+="8";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_nineButton_clicked()
+{
+    expression+="9";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_leftParentheseButton_clicked()
+{
+    expression+="(";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_rightParentheseButton_clicked()
+{
+    expression+=")";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_plusButton_clicked()
+{
+    expression+="+";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_minusButton_clicked()
+{
+    expression+="-";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_timeButton_clicked()
+{
+    expression+="*";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_divideButton_clicked()
+{
+    expression+="/";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_zeroButton_clicked()
+{
+    expression+="0";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_clearButton_clicked()
+{
+    expression="";
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_backSpaceButton_clicked()
+{
+    expression.chop(1);
+    ui->mainLineEdit->setText(expression);
+}
+
+
+void Widget::on_equalButton_clicked()
+{
+
+}
+
---
+++ widget.h
@@ -0,0 +1,63 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_oneButton_clicked();
+
+    void on_twoButton_clicked();
+
+    void on_threeButton_clicked();
+
+    void on_fourButton_clicked();
+
+    void on_fiveButton_clicked();
+
+    void on_sixButton_clicked();
+
+    void on_sevenButton_clicked();
+
+    void on_eightButton_clicked();
+
+    void on_nineButton_clicked();
+
+    void on_leftParentheseButton_clicked();
+
+    void on_rightParentheseButton_clicked();
+
+    void on_plusButton_clicked();
+
+    void on_minusButton_clicked();
+
+    void on_timeButton_clicked();
+
+    void on_divideButton_clicked();
+
+    void on_zeroButton_clicked();
+
+    void on_clearButton_clicked();
+
+    void on_backSpaceButton_clicked();
+
+    void on_equalButton_clicked();
+
+private:
+    Ui::Widget *ui;
+    QString expression;
+};
+#endif // WIDGET_H
